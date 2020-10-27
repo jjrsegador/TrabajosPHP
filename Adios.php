7e5fd9794f6b9d05fca1fa552438d69b130db213
Javier Ramírez <jjrsegador8@gmail.com>
2020-10-27 09:54:46 +0100

    Subida adios.php

---
+++ Adios.php
@@ -0,0 +1,15 @@
+<html>
+
+  <head>
+  </head>
+    <title> Ramirez_Segador_Jose Javier_Entornos1_Caso2 </title>
+	
+  <body>
+
+    <?php
+       echo "Hasta luego desarrollador!!!";
+    ?>
+
+  </body>
+
+</html>
